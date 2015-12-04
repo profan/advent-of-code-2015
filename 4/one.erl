45b89e7543a0293597eba9aeeccda21f1085a359
Robin Hübner <profan@prfn.se>
2015-12-04 16:20:34 +0100

    add completed erlang solution for day 4 part 1

---
+++ 4/one.erl
@@ -0,0 +1,12 @@
+-module(one).
+
+-export([find_hash/2]).
+
+find_hashi(Key, <<0:20, _/bitstring>>, N) ->
+	Key ++ integer_to_list(N);
+
+find_hashi(Base, _, N) ->
+	find_hashi(Base, erlang:md5(Base ++ integer_to_list(N+1)), N+1).
+
+find_hash(Key, N) ->
+	find_hashi(Key, erlang:md5(Key ++ integer_to_list(N)), N).
