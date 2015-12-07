73bfb56d12ebe1087e768803207c1bec84fb9f23
Robin Hübner <profan@prfn.se>
2015-12-07 06:18:08 +0100

    Day 04 - trimming the fat on sequential solution.

--- four.erl
+++ four.erl
@@ -1,17 +1,18 @@
 -module(four).
 -export([find_hash/2, test/0, run/0]).
 
-find_hash(Base, Hash, N, Zeroes) ->
+find_hash(Base, N, Zeroes) ->
 	Bits = Zeroes * 4,
+	Hash = erlang:md5([Base | integer_to_list(N)]),
 	case Hash of
 		<<0:Bits, _/bitstring>> ->
 			N;
 		_ ->
-			find_hash(Base, erlang:md5([Base | integer_to_list(N+1)]), N+1, Zeroes)
+			find_hash(Base, N+1, Zeroes)
 	end.
 
 find_hash(Key, Zeroes) ->
-	find_hash(Key, erlang:md5([Key | integer_to_list(0)]), 0, Zeroes).
+	find_hash(Key, 0, Zeroes).
 
 test() ->
 	609043 = find_hash("abcdef", 5),
