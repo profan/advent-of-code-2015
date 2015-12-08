535022be85b6b1b6a4e13737531b29cbae7e5e26
Robin Hübner <profan@prfn.se>
2015-12-08 03:24:15 +0100

    almost complete, chasing type errors

--- 6-1.ml
+++ 6-1.ml
@@ -20,25 +20,41 @@ let print_set s = PointSet.iter (fun (x, y) -> Printf.printf "(%d, %d)" x y) s
 let tokenize str = 
     let tokens = Core.Core_string.split_on_chars ~on: [' '; ','] str in
         begin match tokens with
-        | a::b::c::d::e::f::g::rest -> (a ^ b, (int_of_string c, int_of_string d), (int_of_string f, int_of_string g))
+        | a::b::c::d::e::f::g::rest -> (a ^ " " ^ b, (int_of_string c, int_of_string d), (int_of_string f, int_of_string g))
         | a::c::d::e::f::g::rest -> (a, (int_of_string c, int_of_string d), (int_of_string f, int_of_string g))
         | [] -> ("", (0, 0), (0, 0))
         | _ -> ("", (0, 0), (0, 0))
         end
 
+let tokenize_lines input = List.map (fun line -> tokenize line) input
+
+let walk_range (x1, y1) (x2, y2) =
+    let x_range = Core.Std.List.range x1 x2 in
+    List.flatten [List.map (fun x ->
+        let y_range = Core.Std.List.range y1 y2 in
+        List.map (fun y -> (x, y)) y_range) x_range]
+
 let read_instructions tokens =
-    begin match tokens with
-    | "turn on"::lower::upper::rest -> "Turn On!"
-    | "turn off"::lower::upper::rest -> "Turn Off!"
-    | "toggle"::lower::upper::rest -> "Toggle!"
-    | [] -> "Nay!"
-    | _ -> "Aww!"
-    end
+    List.fold_left (fun acc x ->
+        begin match x with
+        | ("turn on", lower, upper) ->
+            let path = walk_range lower upper in
+                List.fold_left PointSet.add acc path
+        | ("turn off", lower, upper) ->
+            let path = walk_range lower upper in
+                List.fold_left PointSet.remove acc path
+        | ("toggle", lower, upper) ->
+            let path = walk_range lower upper in
+                List.fold_left (fun acc x -> 
+                    let existence = PointSet.exists x acc in
+                        begin match existence with
+                        | true -> PointSet.remove x acc
+                        | false -> PointSet.add x acc
+                        end) acc path
+        | _ -> acc
+        end) PointSet.empty tokens
 
-let process_lines input = List.map (fun line -> tokenize line) input
-
-let coords = PointSet.empty
 let data = read_lines "6.input";;
-let processed_data = process_lines data;;
+let processed_data = tokenize_lines data;;
 
-Printf.printf "%s" (read_instructions processed_data)
+read_instructions processed_data
