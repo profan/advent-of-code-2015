3e21706f629c3f1671e059bc4a425aaeab0545fe
Robin Hübner <profan@prfn.se>
2015-12-08 05:28:34 +0100

    asd

--- 6-1.ml
+++ 6-1.ml
@@ -11,11 +11,7 @@ module PointSet = Set.Make(
     struct
         let compare = Pervasives.compare
         type t = point
-    end
-)
-
-let read file = read_lines file
-let print_set s = PointSet.iter (fun (x, y) -> Printf.printf "(%d, %d)" x y) s
+    end)
 
 let tokenize str = 
     let tokens = Core.Core_string.split_on_chars ~on: [' '; ','] str in
@@ -28,11 +24,11 @@ let tokenize str =
 
 let tokenize_lines input = List.map (fun line -> tokenize line) input
 
-let walk_range (x1, y1) (x2, y2) =
+let walk_range (x1, y1) (x2, y2) : point list =
     let x_range = Core.Std.List.range x1 x2 in
-    List.flatten [List.map (fun x ->
+    List.flatten (List.map (fun x ->
         let y_range = Core.Std.List.range y1 y2 in
-        List.map (fun y -> (x, y)) y_range) x_range]
+        List.map (fun y -> (x, y)) y_range) x_range)
 
 let read_instructions tokens =
     List.fold_left (fun acc x ->
@@ -54,7 +50,9 @@ let read_instructions tokens =
         | _ -> acc
         end) PointSet.empty tokens
 
+let print_set s = PointSet.iter (fun (x, y) -> Printf.printf "(%d, %d)" x y) s
+
+(* begin the processings *)
 let data = read_lines "6.input";;
 let processed_data = tokenize_lines data;;
-
 read_instructions processed_data
